c9acec3f429bcbd54ac31ec126bff9a7afbfdefb
Sjaak <sjaakrest@gmail.com>
2021-02-02 11:47:41 +0100

    les 1

---
+++ DEA/Week1/Les1/FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        fizzBuzz(100);
+    }
+
+    private static void fizzBuzz(int num) {
+        for(int i = 1; i <= num; i++) {
+            if (i % 5 == 0 && i % 3 ==0)
+                System.out.println("FizzBuzz ");
+            else if(i % 5 == 0)
+                System.out.println("Buzz ");
+            else if(i % 3 == 0)
+                System.out.println("Fizz ");
+            else
+                System.out.println(i);
+        }
+    }
+}
---
+++ DEA/Week1/Les1/maventest/src/main/java/han/oose/dea/App.java
@@ -0,0 +1,13 @@
+package han.oose.dea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
